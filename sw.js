429aa6ca6daaf1350c10a14175dba5ccd6f56eca
octo701028-sudo <octo701028@gmail.com>
2025-08-26 13:12:25 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 
-self.addEventListener('install',e=>{e.waitUntil(caches.open('e7v72').then(c=>c.addAll([
-  './','./index.html','./manifest.json','./icon-192.png','./icon-512.png',
-  './stages.json','./transitions.json','./oracle_whispering.json','./yl_blends.json','./hidden_cards.json'
-])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open('enervi7-v3').then(c=>c.addAll([
+    './','./index.html','./manifest.json','./sw.js','./yl_blends.json',
+    './oracle_whispering.json','./hidden_cards.json','./icon-192.png','./icon-512.png'
+  ])));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
